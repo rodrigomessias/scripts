af645910ccc1bde55151d7c664c2465b97cd16e8
Rodrigo Messias <git@rmessias.com>
2023-05-19 14:58:11 -0300

    make killall prettier

--- kill/killall.fish
+++ kill/killall.fish
@@ -8,105 +8,106 @@
 # License: GPL v3 or later.  See LICENSE
 #
 # browsers
-echo "# browsers"
+echo " browsers"
 if pgrep waterfox >/dev/null                                                                                
   wmctrl -c waterfox
-  echo "closing waterfox"
+  echo "  closing waterfox"
 else
-  echo "waterfox not open"
+  echo "   waterfox not open"
 end
 sleep 0.1
 
 if pgrep -f /usr/lib/firefox/firefox >/dev/null                                                                                
   wmctrl -c "Mozilla Firefox"
-  echo "closing firetube"
+  echo "  closing firetube"
 else
-  echo "firetube not open"
+  echo "   firetube not open"
 end
 sleep 0.1
 
 if pgrep -f firefoxdeveloperedition >/dev/null                                                                                
   wmctrl -c "Firefox Developer"
-  echo "closing firefoxdev"
+  echo "  closing firefoxdev"
 else
-  echo "firefoxdev not open"
+  echo "   firefoxdev not open"
 end
 sleep 0.1
 
 # chat
 echo ""
-echo "# chat"
+echo " chat"
 if pgrep -f whatsapp >/dev/null                                                                                
   wmctrl -c WhatsApp
-  echo "closing whatsapp"
+  echo "  closing whatsapp"
 else
-  echo "whatsapp not open"
+  echo "   whatsapp not open"
 end
 sleep 0.1
 
 if pgrep -f telegram >/dev/null                                                                                
   wmctrl -c Telegram
-  echo "closing telegram"
+  echo "  closing telegram"
 else
-  echo "telegram not open"
+  echo "   telegram not open"
 end
 sleep 0.1
 
 # organizers
 echo ""
-echo "# organizers"
+echo " organizers"
 if pgrep -f obsidian >/dev/null                                                                                
   wmctrl -c Obsidian
-  echo "closing notepad.md"
+  echo "  closing notepad.md"
 else
-  echo "notepad.md not open"
+  echo "   notepad.md not open"
 end
 sleep 0.1
 
 if pgrep -f todoist >/dev/null                                                                                
   wmctrl -c todoist
-  echo "closing todoist"
+  echo "  closing todoist"
 else
-  echo "todoist not open"
+  echo "   todoist not open"
 end
 sleep 0.1
 
 if pgrep -x kalendar >/dev/null                                                                                
   wmctrl -c kalendar
-  echo "closing kalendar"
+  echo "  closing kalendar"
 else
-  echo "kalendar not open"
+  echo "   kalendar not open"
 end
 sleep 0.1
 
 # media
 echo ""
-echo "# media"
+echo "辶 media"
 if pgrep mpv >/dev/null                                                                                
   killall mpv
-  echo "closing mpv"
+  echo "  closing mpv"
 else
-  echo "mpv not open"
+  echo "   mpv not open"
 end
 sleep 0.1
 
 if pgrep streamlink >/dev/null                                                                                
   killall streamlink
   wmctrl -c Streamlink
-  echo "closing streamlink"
+  echo "  closing streamlink"
 else
-  echo "streamlink not open"
+  echo "   streamlink not open"
 end
 sleep 0.1
 
 if pgrep musikcube >/dev/null                                                                                
   kill musikcube
-  echo "closing musikcube"
+  echo "  closing musikcube"
 else
-  echo "musikcube not open"
+  echo "   musikcube not open"
 end
 sleep 0.1
 
+echo ""
 echo ""
 echo "✔ done"
 sleep 2
